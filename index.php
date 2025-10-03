8acf603a64dd91d631513750068bebe12c113910
Robin Pibouleau Lacoste <robin.pibouleau-lacoste9@etu.univ-lorraine.fr>
2025-10-03 16:59:25 +0200

    modif du index pour le lancement

--- index.php
+++ index.php
@@ -1,26 +1,8 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-use IUT\Deefy\DefaultAction;
-use IUT\Deefy\DisplayPlaylistAction;
-use IUT\Deefy\AddPlaylistAction;
-use IUT\Deefy\AddPodcastTrackAction;
 
-$action = $_GET['action'] ?? 'default';
+use IUT\Deefy\Dispatcher;
 
-switch ($action) {
-    case 'playlist':
-        $actionObject = new DisplayPlaylistAction();
-        break;
-    case 'add-playlist':
-        $actionObject = new AddPlaylistAction();
-        break;
-    case 'add-track':
-        $actionObject = new AddPodcastTrackAction();
-        break;
-    default:
-        $actionObject = new DefaultAction();
-        break;
-}
-
-echo $actionObject->execute(); 
+$dispatcher = new Dispatcher();
+$dispatcher->run();
