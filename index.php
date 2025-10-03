23ee571a9fff08a21e48ebc55a095dfe44b2fb7f
Robin Pibouleau Lacoste <robin.pibouleau-lacoste9@etu.univ-lorraine.fr>
2025-10-03 17:02:41 +0200

    Ajout de l'index

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-<<<<<<< HEAD
-=======
-use IUT\Deefy\Action\AddPlaylistAction;
-use IUT\Deefy\Action\AddPodcastTrackAction;
-use IUT\Deefy\Action\DefaultAction;
-use IUT\Deefy\Action\DisplayPlaylistAction;
->>>>>>> 2601944a97885aae5f809984057f753dc1de3a5c
 
 use IUT\Deefy\Dispatcher;
 
