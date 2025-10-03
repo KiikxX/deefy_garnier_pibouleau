6eec2fcaf8dabb7a6338f604fdcd0077980c576c
Kiryan Garnier <kiryangarnier@gmail.com>
2025-10-03 15:07:49 +0200

    ajout du vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'iutspotify/td5',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c5abfadb38c8d4751dfe9e35fd0737e107024f10',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'iutspotify/td5' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c5abfadb38c8d4751dfe9e35fd0737e107024f10',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
